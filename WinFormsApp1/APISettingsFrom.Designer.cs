505f732c89890f6c09a13f79b3170c1c6e32f8e6
ArenVardanyan <a.vardanyan00101@gmail.com>
2025-11-18 16:41:20 +0300

    add some components to form 'APISettingsForm'

--- WinFormsApp1/APISettingsFrom.Designer.cs
+++ WinFormsApp1/APISettingsFrom.Designer.cs
@@ -28,18 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F);
+            label1.Location = new Point(12, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Домен:";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(77, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(328, 25);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.Location = new Point(12, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 19);
+            label2.TabIndex = 2;
+            label2.Text = "Логин:";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 10F);
+            textBox2.Location = new Point(77, 83);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(328, 25);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.Location = new Point(12, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 19);
+            label3.TabIndex = 4;
+            label3.Text = "Пароль:";
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Location = new Point(77, 130);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(328, 25);
+            textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(144, 175);
+            button1.Name = "button1";
+            button1.Size = new Size(157, 34);
+            button1.TabIndex = 6;
+            button1.Text = "Связать";
+            button1.UseVisualStyleBackColor = true;
             // 
             // APISettingsFrom
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(563, 219);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 10F);
             Name = "APISettingsFrom";
             Text = "Настройки API";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Button button1;
     }
 }
--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa535661102a6721841c10d58ba2c0b3e294eee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06793251dd7d6389ee83e9dc9639bfa5d5d00b88")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
