07990dfec60a01b07322bccc6146ebfdf43cb369
ArenVardanyan <a.vardanyan00101@gmail.com>
2025-11-18 22:49:41 +0300

    add override method ToString() for Client

--- WinFormsApp1/Models.cs
+++ WinFormsApp1/Models.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace WinFormsApp1
 {
-    internal class Models
+    public class ApiSettings
     {
+        public string Domain { get; set; } = "";
+        public string Token { get; set; } = "";
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName}";
+        }
+    }
+
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; } = "";
+        public int TypeId { get; set; }
+        public int BreedId { get; set; }
+        public string Sex { get; set; } = "";
     }
 }
