6ce0f9d183f0ce3227d97edf9d59cc9e149dc4c8
ArenVardanyan <a.vardanyan00101@gmail.com>
2026-02-02 18:26:13 +0300

    small refactoring

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -2,16 +2,9 @@ namespace WinFormsApp1
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // Добавьте это для отладки
-            Console.WriteLine("=== Запуск приложения ===");
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
